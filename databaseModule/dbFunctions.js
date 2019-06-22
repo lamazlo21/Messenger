ac57dc25183af9bf896c3b81ff9b016c3d148ac3
Dawid <lamazlo21@gmail.com>
2019-06-23 00:22:16 +0200

    Database functions are waiting to be repaired

--- databaseModule/dbFunctions.js
+++ databaseModule/dbFunctions.js
@@ -1,3 +1,21 @@
 //let addUser = (name, surname, password)=>{
   //  db.query('INSERT INTO users SET ?', )
 //}
+
+// GET all rooms with number of users signed up to this room
+
+async function getAllRooms(db){
+     await db.query('SELECT name,count(room) as `numberOfUsers` FROM' +
+        ' `Rooms`LEFT OUTER JOIN `Room users` ON' +
+        ' Rooms.room_id = `Room users`.`room` GROUP BY room_id',
+        (err, result, fields)=>{
+        if(err)
+            throw err;
+        return result
+    })
+    console.log("xd")
+
+}
+
+
+module.exports.getAllRooms = getAllRooms;
